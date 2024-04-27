8120bf63cd35c9a69dd45d9a1c0e62f6b4889792
ameliatuttle <aetuttle@byui.edu>
2024-04-27 09:52:22 -0600

    Initial Commit

---
+++ Lab01/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab01
+//
+//  Created by Amelia Tuttle on 4/27/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
