cb4d7b0c98b7aed6678488f2a24be6317b25f386
ameliatuttle <aetuttle@byui.edu>
2024-04-29 17:17:52 -0600

    hello?

--- Lab01/main.cpp
+++ Lab01/main.cpp
@@ -5,10 +5,32 @@
 //  Created by Amelia Tuttle on 4/27/24.
 //
 
-#include <iostream>
+/*************************************************************
+ * 1. Name:
+ *      Amelia Tuttle
+ * 2. Assignment Name:
+ *      Lab 01: Hello World
+ * 3. Assignment Description:
+ *      A simple program to display a message on the screen
+ * 4. What was the hardest part? Be as specific as possible.
+ *      The hardest part of this assignment was installing xcode and learning the new IDE. I
+ *      have used it before in in a class I took on C, but I uninstalled it because I thought I wouldn't
+ *      need it again (lesson learned). I never got very used to it, but I'm excited to try again and get
+ *      more familiar with the space. I have also never coded in C++ so that will be a bit of a challlenge.
+ * 5. How long did it take for you to complete the assignment?
+ *       1 hour
+ *****************************************************************/
 
+#include <iostream>
+using namespace std;
+
+/***********************
+ * Main
+ **********************************/
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // Display "Hello World!"
+    std::cout << "Hello World!\n";
+    // Return the number 0
     return 0;
 }
+hello
